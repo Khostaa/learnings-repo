2c22a58206bc053ee227709f881bb6a735134fc9
khostaa <sunilpoudel668@gmail.com>
2024-10-01 23:28:49 +0545

    Completed question 1/50

--- oddeven.py
+++
@@ -1,9 +0,0 @@
-# Write a program to check whether a number is odd or even.
-x = int(input("Enter a number: "))
-if x >= 0:
-    if x % 2 == 0:
-        print(f"{x} is a even number.")
-    else:
-        print(f"{x} is a odd number.")
-else:
-    print("Invalid input")
